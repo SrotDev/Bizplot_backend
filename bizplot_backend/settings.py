d7f1c7285da4f54ad28952986d29b7a841549160
Ragib Shahrier Bin Kabir <ragibshahrier2004123@gmail.com>
2025-09-24 05:48:41 +0600

    add schema

--- bizplot_backend/settings.py
+++ bizplot_backend/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'rest_framework.authtoken',
     "rest_framework_simplejwt.token_blacklist",  # for logout/refresh
+    'drf_spectacular',
     
     # "dj_rest_auth",                        # REST auth helpers
     # "dj_rest_auth.registration",           # registration endpoints (optional)
@@ -80,6 +81,15 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": (
         "rest_framework.permissions.IsAuthenticated",
     ),
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Your Project API',
+    'DESCRIPTION': 'Your project description',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+    # OTHER SETTINGS
 }
 
 
